05de6ca091c34c3f394d20fd272718dba4b5b036
Vincent Robert Weis <vrobweis@gmail.com>
2020-10-20 13:56:49 -0700

    confirmed refactoring of SufferShop P0 project, committed deletion of old sample project

--- StoreApp_VRW_P0/StoreBL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace StoreBL
-{
-    public class Class1
-    {
-    }
-}
--- StoreApp_VRW_P0/StoreModels/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace StoreModels
-{
-    public class Class1
-    {
-    }
-}
